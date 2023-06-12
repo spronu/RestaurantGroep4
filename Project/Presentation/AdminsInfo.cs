4ebd14a86d0c5b4e45ebe6cec620bcdebce49b97
RayanAIR1 <1056487@hr.nl>
2023-06-12 15:27:59 +0200

    admin heeft een overzicht van alle reserveringen op specifieke dagen en kan die wijzigen

--- Project/Presentation/AdminsInfo.cs
+++ Project/Presentation/AdminsInfo.cs
@@ -1,20 +1,55 @@
-public class ReservationInfo
+public class AdminsInfo
 {
     private static ReservationLogic reservationLogic = new ReservationLogic();
+    public DateTime currentDate = DateTime.Today;
 
-    public static void ShowReservationInfo()
+    public bool currentStatus = false;
+
+    public void ShowReservationInfo()
     {
         var getdata = reservationLogic.GetAll();
 
-        getdata = getdata.FindAll(i => i.AccountId == AccountsLogic.CurrentAccount.Id);
+        getdata = getdata.FindAll(i => i.ReservationDateTime.Date == currentDate);
 
         if (getdata.Count == 0)
         {
+            int index = 0;
             Console.Clear();
-            Console.WriteLine("U heeft geen reserveringen");
-            Console.WriteLine("U wordt terug geleid naar het menu");
-            Thread.Sleep(1500);
-            Menu.Start();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("====================================");
+            Console.WriteLine("|     Reserverings informatie      |");
+            Console.WriteLine("====================================");
+            Console.WriteLine($"{currentDate.ToString("dd-MM-yyyy")}");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("Er zijn geen reserveringen op deze dag.");
+            Console.WriteLine(
+                "Gebruik de pijltjestoetsen om te navigeren. Druk op Enter om terug te keren naar het menu."
+            );
+
+            var key = Console.ReadKey(true);
+            switch (key.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    if (index > 0)
+                        index--;
+                    break;
+                case ConsoleKey.DownArrow:
+                    if (index < getdata.Count - 1)
+                        index++;
+                    break;
+                case ConsoleKey.RightArrow:
+                    currentDate = currentDate.AddDays(1);
+                    return;
+                case ConsoleKey.LeftArrow:
+                    currentDate = currentDate.AddDays(-1);
+                    return;
+                case ConsoleKey.Enter:
+                    currentStatus = true;
+                    return;
+                
+            }
+
         }
         else
         {
@@ -26,6 +61,7 @@ public class ReservationInfo
                 Console.WriteLine("====================================");
                 Console.WriteLine("|     Reserverings informatie      |");
                 Console.WriteLine("====================================");
+                Console.WriteLine($"{currentDate.ToString("dd-MM-yyyy")}");
                 Console.ResetColor();
                 Console.WriteLine();
 
@@ -65,7 +101,12 @@ public class ReservationInfo
                         if (index < getdata.Count - 1)
                             index++;
                         break;
-                    
+                    case ConsoleKey.RightArrow:
+                        currentDate = currentDate.AddDays(1);
+                        return;
+                    case ConsoleKey.LeftArrow:
+                        currentDate = currentDate.AddDays(-1);
+                        return;
                     case ConsoleKey.E:
                         // Console.WriteLine("selectie elementen menu + verwijder optie");
                         // Thread.Sleep(2000);
@@ -75,8 +116,8 @@ public class ReservationInfo
                         getdata = reservationLogic.GetAll();
                         break;
                     case ConsoleKey.Enter:
-                        Menu.Start();
-                        break;
+                        currentStatus = true;
+                        return;
                 }
             } while (true);
         }
