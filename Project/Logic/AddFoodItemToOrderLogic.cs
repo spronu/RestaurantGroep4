427e560b41b43053ec30bf96cbb4330e005a886a
Spronu <yildizyahya.38@gmail.com>
2023-06-12 11:14:30 +0200

    duidelijke Logic namen gemaakt en References aangepast zodat het nu wel goed wordt aangeroepen

--- Project/Logic/AddFoodItemToOrderLogic.cs
+++ Project/Logic/AddFoodItemToOrderLogic.cs
@@ -6,7 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Threading;
 
-static class CorrectInputCheck
+static class AddFoodItemToOrderLogic
 {
 
     static ReservationLogic reservationlogics = new ReservationLogic();
