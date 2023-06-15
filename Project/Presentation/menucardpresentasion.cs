236db3e631bec414023879990ef0a714a103ef5d
pieterbaas4 <pvspijk@gmail.com>
2023-06-15 15:41:48 +0200

    aanpasssing verwijder gerecht en nieuw gerecht

--- Project/Presentation/menucardpresentasion.cs
+++ Project/Presentation/menucardpresentasion.cs
@@ -1,35 +1,33 @@
 public class menucardpresentasion
 {
-    public static bool menucard(
-        bool showcourse,
-        List<MenuItems> ListmenuItems,
-        bool selection = false
-    )
+    public static bool menucard(bool showcourse, List<MenuItems> ListmenuItems,ThemeItem activetheme = null, bool selection = false)
     {
-        ThemeItem activetheme = GiveThemeLogic.GetActiveTheme();
-        Tuple<string, string> all = MenuDataLogic.MakeOptionsLists(showcourse);
 
+        if (activetheme == null){
+            activetheme = GiveThemeLogic.GetActiveTheme();
+        }
+        Tuple<string, string> all = MenuDataLogic.MakeOptionsLists(showcourse);
+        
+        // Console.WriteLine(all);
         string option = all.Item1;
         string course = all.Item2;
+        // string course = MenuDataLogic.hallo().Item2;
+
+
 
         if (!selection)
         {
-            if (option != "quit")
+        if (option != "quit")
             {
                 Console.WriteLine(option);
                 Console.Clear();
 
                 foreach (MenuItems item in ListmenuItems)
                 {
-                    if (
-                        item.category.ToString() == option
-                        && item.course.ToString() == course
-                        && item.thema == activetheme.Theme.ToString()
-                    )
+                    // Console.WriteLine(item["course"].ToString());
+                    if (item.category.ToString() == option && item.course.ToString() == course && item.thema == activetheme.Theme.ToString())
                     {
-                        Console.WriteLine(
-                            $"{item.id.ToString()}. {item.name.ToString().PadRight(30)}   :  ${item.price.ToString().PadRight(10)}  [ {item.course.ToString()} ]"
-                        );
+                        Console.WriteLine($"{item.id.ToString()}. {item.name.ToString().PadRight(30)}   :  ${item.price.ToString().PadRight(10)}  [ {item.course.ToString()} ]");
                     }
                 }
                 Console.WriteLine("");
@@ -37,13 +35,16 @@ public class menucardpresentasion
             }
         }
 
+        // Console.WriteLine(food["name"]); manier om de prijs te printen
         return false;
     }
-
-    public static string GetNewoption()
-    {
+    
+    public static string GetNewoption(){
         Console.WriteLine(" druk enter om iets anders te zien.");
         string option = Console.ReadLine() ?? string.Empty;
         return option;
     }
 }
+
+
+
